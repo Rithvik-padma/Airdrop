0f52923c3c1c458ba7673115279cbc9382d7b642
Rithvik-padma <rithvik.padma@gmail.com>
2023-02-01 20:22:00 +0530

    Modify airdrop files

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -2,18 +2,19 @@ pragma solidity >=0.8.0 <0.9.0;
 
 contract Airdrop{
     address[] public recipients;
-    constructor(){
-        balance();
-    }
-    function balance() internal view returns(uint){
-        return address(this).balance;
+    address public admin;
+    constructor(){ 
+        admin = msg.sender;
     }
     receive() external payable{
-        require(msg.value == 0.1 ether, "Only 0.1 ether is accepted by the contract");
-        require(address(this).balance > 10.1 ether, "Insufficient balance!");
-        require(check(msg.sender) == true, "Only allowed once!");
-        payable(msg.sender).transfer(10 ether);
+        require(msg.sender != admin, "Admin cannot transact!");
+        require(msg.value == 0.1 ether || msg.value == 50 ether,"Exactly 0.1 ether should be transacted");
+        require(address(this).balance > 1 ether, "Insufficient balance!");
+        if(msg.value == 0.1 ether){
+        require(!check(msg.sender), "Only allowed once!");
+        payable(msg.sender).transfer(1 ether);
         recipients.push(msg.sender);
+        }
     }
     function check(address current) internal view returns(bool){
         for(uint i =0; i<recipients.length; i++){
